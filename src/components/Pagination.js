d96ae3e9574e8eef008cba384f8ebfff2ff0180e
Kaaarlos <ca991467@gmail.com>
2022-07-07 19:17:52 -0500

    first commit

---
+++ src/components/Pagination.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const pagination = ({ prev, next, onPrevious, onNext }) => {
+  const handlePrevious = () => {
+    onPrevious();
+  };
+  const handleNext = () => {
+    onNext();
+  };
+
+  return (
+    <nav className="my-5">
+      <ul className="pagination justify-content-center">
+        {prev ? (
+          <li className="page-item">
+            <button className="page-link" onClick={handlePrevious}>
+              Previous
+            </button>
+          </li>
+        ) : null}
+
+        {next ? (
+          <li className="page-item">
+            <button className="page-link" onClick={handleNext}>
+              Next
+            </button>
+          </li>
+        ) : null}
+      </ul>
+    </nav>
+  );
+};
+
+export default pagination;
